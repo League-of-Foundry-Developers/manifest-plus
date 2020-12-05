0a40b406b8ef914765121f44a94d37c2bdba323f
ardittristan <ardittristan@gmail.com>
2020-12-05 22:18:15 +0100

    permissions

--- .github/workflows/helpers/append.js
+++ .github/workflows/helpers/append.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const moment = require("moment");
 
 fs.writeFileSync(
-  "pages/index.html",
+  "./pages/index.html",
   `<li><a href="https://github.com/${process.env.GITHUB_REPOSITORY}/blob/gh-pages/${process.env.FOLDER_NAME}/wiki-article.md">${moment().format("DD-MM-YYYY")}</a></li>`,
   { flag: "a" }
 );
