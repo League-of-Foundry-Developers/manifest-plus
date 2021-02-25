d1061494c14e701d2f3850de9ddcbfe8ea731606
typhonrt <support@typhonrt.org>
2021-02-24 23:40:26 -0800

    update for v1.1.0 and gitdown

--- .github/workflows/helpers/append.js
+++ .github/workflows/helpers/append.js
@@ -4,6 +4,6 @@ const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/")
 
 fs.writeFileSync(
   "./pages/index.md",
-  `* [${moment().format("DD-MM-YYYY")}](https://${owner}.github.io/${repo}/${process.env.FOLDER_NAME}/wiki-article.html)`,
+  `* [${moment().format("DD-MM-YYYY")}](https://${owner}.github.io/${repo}/${process.env.FOLDER_NAME}/ManifestPlus.html)`,
   { flag: "a" }
 );
---
+++ README.md
@@ -0,0 +1,5 @@
+Provides a repository for the League of Extraordinary Foundry Developers Manifest+ specification. 
+
+The current specification version is [v1.1.0](ManifestPlus.md)
+
+The specification is split into sections found in ./specification
