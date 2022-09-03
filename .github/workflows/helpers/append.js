cafa270f31ad9d058ccbcb5db7cfc81c063e0621
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2022-09-03 14:32:12 -0400

    Archive Manifest+ repo in favor of wiki page

--- .github/workflows/helpers/append.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const moment = require("moment");
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/")
-
-fs.writeFileSync(
-  "./pages/index.md",
-  `\n* [${moment().format("DD-MM-YYYY")}](https://${owner}.github.io/${repo}/${process.env.FOLDER_NAME}/ManifestPlus.html)`,
-  { flag: "a" }
-);
--- .github/workflows/helpers/get-name.js
+++
@@ -1,2 +0,0 @@
-const moment = require('moment')
-console.log(moment().format('YYYY-MM-DD-HH-mm'))
